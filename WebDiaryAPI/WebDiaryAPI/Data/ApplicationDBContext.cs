4c92d3121889a98b7e7bd208681c8d77ac17f95e
Whigle <i.bro.n.man@gmail.com>
2026-05-11 11:17:02 +0200

    added seeding method for diary and migrations

--- WebDiaryAPI/WebDiaryAPI/Data/ApplicationDBContext.cs
+++ WebDiaryAPI/WebDiaryAPI/Data/ApplicationDBContext.cs
@@ -11,5 +11,16 @@ namespace WebDiaryAPI.Data
 		}
 
 		public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<DiaryEntry>().HasData(
+				new DiaryEntry { Id = 1, Title = "Went Hiking", Content = "Went hiking with Joe!", Created = new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4264) },
+				new DiaryEntry { Id = 2, Title = "Went Shoping", Content = "Went shoping with Joe!", Created = new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4391) },
+				new DiaryEntry { Id = 3, Title = "Went Diving", Content = "Went diving with Joe!", Created = new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4395) }
+			);
+		}
 	}
 }
---
+++ WebDiaryAPI/WebDiaryAPI/Migrations/20260511091616_AddSeedDiaryEntries.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebDiaryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedDiaryEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaryEntries", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4264), "Went Hiking" },
+                    { 2, "Went shoping with Joe!", new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4391), "Went Shoping" },
+                    { 3, "Went diving with Joe!", new DateTime(2026, 4, 18, 13, 9, 29, 41, DateTimeKind.Local).AddTicks(4395), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaryEntries");
+        }
+    }
+}
